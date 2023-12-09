28afbdf494748444c9462876e1a7ee6d4a417ab3
mmf.valenchi <mmf.valenchi@MMF403-14>
2023-12-09 13:48:32 +0300

    comm

--- TestProject1/UnitTestCalculator.cs
+++ TestProject1/UnitTestCalculator.cs
@@ -52,7 +52,7 @@ public class Tests
         const double tolerance = 1e-200;
 
 
-        Assert.That(Math.Abs(actualDouble/expected-1), Is.LessThanOrEqualTo(tolerance));
+             Assert.That(Math.Abs(actualDouble/expected-1), Is.LessThanOrEqualTo(tolerance));
     }
 
     [TestCase(" 2 +3 ", 5)]
